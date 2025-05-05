b15c14bb5d8ba4b03b41278f2debddfd146dac31
Marcos Magno <marcosmagnopb@gmail.com>
2025-05-05 20:41:04 -0300

    refactor: remove header from Home component in Organograma page

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,6 @@ export default function Home() {
   return (
     <div>
       <main className="container mx-auto p-4 max-w-6xl">
-        <h1 className="text-3xl font-bold text-center my-6">
-          Organograma Hierárquico
-        </h1>
         <OrganogramaView />
       </main>
       <Toaster />
